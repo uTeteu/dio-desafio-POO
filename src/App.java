6acca94bc8cd42c4d75488a60d4d11edec155adc
Teteu <matheuskiper96@gmail.com>
2025-02-12 16:58:17 -0300

    add Curso e Mentoria

--- src/App.java
+++ src/App.java
@@ -1,5 +1,29 @@
+import java.time.LocalDate;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
 public class App {
     public static void main(String[] args) throws Exception {
 
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso JS");
+        curso2.setDescricao("Descrição curso JS");
+        curso2.setCargaHoraria(4);
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria de Java");
+        mentoria1.setDescricao("Descrição mentoria Java");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(mentoria1);
     }
 }
